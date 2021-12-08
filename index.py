acb140d43fde37cd128f5f27f1765b6b56442b2f
KimHanyi <kimhanyi1229@gmail.com>
2021-12-08 15:48:02 +0900

    v2

--- index.py
+++ index.py
@@ -4,31 +4,53 @@
 from flask import Flask, request
 from flask import render_template
 import RPi.GPIO as GPIO
+import time
 
 app = Flask(__name__)
-GPIO.setmode(GPIO.BOARD)                    #BOARD는 커넥터 pin번호 사용
-pin=7                                       # 7번 pin. 즉, GPIO4 사용
-GPIO.setup(pin, GPIO.OUT, initial=GPIO.LOW) 
+GPIO.setmode(GPIO.BCM)                    #BOARD는 커넥터 pin번호 사용
+TRIG = 23
+ECHO = 24
+print("Distance measurement in progress")
+
+#Trig와 Echo 핀의 출력/입력 설정 
+GPIO.setup(TRIG,GPIO.OUT)
+GPIO.setup(ECHO,GPIO.IN)
+
+#Trig핀의 신호를 0으로 출력 
+GPIO.output(TRIG, False)
+print("Waiting for sensor to settle")
+time.sleep(2)
+
 
 @app.route("/")
 def home():
     return render_template('index.html')
 
-@app.route("/led/on")                       # index.html에서 이 주소를 접속하여 해당 함수를 실행
+@app.route("/md")                       # index.html에서 이 주소를 접속하여 해당 함수를 실행
 def led_on():
-    try:
-        GPIO.output(pin, GPIO.HIGH)         # 불을 켜고
-        return "ok"                         # 함수가 'ok'문자열을 반환함
+    try:			     
+        GPIO.output(TRIG, True)   # Triger 핀에  펄스신호를 만들기 위해 1 출력
+        time.sleep(0.00001)       # 10µs 딜레이 
+        GPIO.output(TRIG, False)
+        
+        while GPIO.input(ECHO)==0:
+            start = time.time()	 # Echo 핀 상승 시간 
+        while GPIO.input(ECHO)==1:
+            stop= time.time	 # Echo 핀 하강 시간 
+            
+        check_time = stop - start
+        distance = check_time * 34300 / 2
+        return str(distance)                         # 함수가 'ok'문자열을 반환함
     except :
         return "fail"
 
-@app.route("/led/off")
-def led_off():
-    try:
-        GPIO.output(pin, GPIO.LOW)
-        return "ok"
-    except :
-        return "fail"
+# @app.route("/led/off")
+# def led_off():
+#     try:
+#         GPIO.output(pin, GPIO.LOW)
+#         return "ok"
+#     except :
+#         return "fail"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
